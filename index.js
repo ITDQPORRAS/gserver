f9ed81258048dc3b607ba1a590e2490b062da14e
ITDQPORRAS <42963615+ITDQPORRAS@users.noreply.github.com>
2020-09-08 20:38:35 +0800

    update

--- index.js
+++ index.js
@@ -2,38 +2,42 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-var server = app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+var server = app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 var io = require('socket.io')(server);
 
 app.use(express.static("./views"));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
-io.on('connection', function (socket) {
-  socket.once('disconnect', function () {
-    console.log(`A user disconnected with socket id ${socket.id}`)
-  });
-  socket.broadcast.emit('userconnected', socket.id);
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', socket.id)
-  })
-  socket.on('nudge-client', data => {
-    socket.broadcast.to(data.to).emit('client-nudged', data)
-  })
-  socket.on('login', data => {
-    socket.broadcast.emit('logins', data)
-  })
-  socket.on('on_ApprovedRR', data => {
-    socket.broadcast.emit('approvedRR', data)
-  })
-  socket.on('approvedTR', data => {
-    socket.broadcast.emit('approvedTR', data)
-  })
+io.on('connection', function(socket) {
+    socket.once('disconnect', function() {
+        console.log(`A user disconnected with socket id ${socket.id}`)
+    });
+    socket.broadcast.emit('userconnected', socket.id);
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', socket.id)
+    })
+    socket.on('nudge-client', data => {
+        socket.broadcast.to(data.to).emit('client-nudged', data)
+    })
+    socket.on('login', data => {
+        socket.broadcast.emit('logins', data)
+    })
+    socket.on('on_ApprovedRR', data => {
+        socket.broadcast.emit('approvedRR', data)
+    })
+    socket.on('approvedTR', data => {
+        socket.broadcast.emit('approvedTR', data)
+    })
+    socket.on('sendMessage', data => {
+        console.log(data)
+        io.to(data.email).emit('receivedMessage', data);
+    })
 });
